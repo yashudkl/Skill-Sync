a957e2dcc346450fe159f0bf465c38efba5521aa
unknown <sujanbhatta0629@gmail.com>
2024-12-05 14:40:38 +0545

    ui-done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import './index.css'
 import MainRouter from './router/router'
 
 
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MainRouter />
--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SetupDescriptionPage from "../pages/SetupAccount/description";
+import SetupLoginPage from "../pages/SetupAccount/login";
+import SetupRegisterPage from "../pages/SetupAccount/register";
+import SetupSkillSyncPage from "../pages/SetupAccount/skill_sync";
 
-export default function MainRouter()
-{
+export default function MainRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/setup" element = {<SetupDescriptionPage />} />
+                <Route path="/setup" element={<SetupDescriptionPage />} />
+                <Route path="/login" element={<SetupLoginPage />} />
+                <Route path="/register" element={<SetupRegisterPage />} />
+                <Route path="/skill_sync" element={<SetupSkillSyncPage />} />
             </Routes>
         </BrowserRouter>
-    )
+    );
 }
