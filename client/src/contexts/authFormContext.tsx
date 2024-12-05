a64186e34a13f92dda9cd5a9793281bdcd004a5c
BiLJX <billjeshbaidya@gmail.com>
2024-12-05 15:00:03 +0545

    axios added

--- client/src/contexts/authFormContext.tsx
+++ client/src/contexts/authFormContext.tsx
@@ -1,31 +1,33 @@
 import React, { createContext, useContext, useState, ReactNode } from "react";
+import myAxios from "../api/axios";
 
 interface LoginState {
-  user_name: string;
-  password: string;
+    user_name: string;
+    password: string;
 }
 
 interface SignupState {
-  full_name: string;
-  email: string;
-  user_name: string;
-  password: string;
+    full_name: string;
+    email: string;
+    user_name: string;
+    password: string;
 }
 
 interface SetupState {
-  bio: string;
-  skills: string[];
-  pfp_url: string;
-  pfp_file: File | null;
+    bio: string;
+    skills: string[];
+    pfp_url: string;
+    pfp_file: File | null;
 }
 
 interface AuthContextType {
-  loginState: LoginState;
-  setLoginState: React.Dispatch<React.SetStateAction<LoginState>>;
-  signupState: SignupState;
-  setSignupState: React.Dispatch<React.SetStateAction<SignupState>>;
-  setupState: SetupState;
-  setSetupState: React.Dispatch<React.SetStateAction<SetupState>>;
+    loginState: LoginState;
+    setLoginState: React.Dispatch<React.SetStateAction<LoginState>>;
+    signupState: SignupState;
+    setSignupState: React.Dispatch<React.SetStateAction<SignupState>>;
+    setupState: SetupState;
+    setSetupState: React.Dispatch<React.SetStateAction<SetupState>>;
+    login: ()=>Promise<any>;
 }
 
 // Create the context with a default value of undefined (we'll check for that)
@@ -33,31 +35,35 @@ const AuthFormContext = createContext<AuthContextType | undefined>(undefined);
 
 // The AuthFormContextProvider component will provide the values to the context
 export const AuthFormContextProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [loginState, setLoginState] = useState<LoginState>({ user_name: "", password: "" });
-  const [signupState, setSignupState] = useState<SignupState>({ full_name: "", email: "", user_name: "", password: "" });
-  const [setupState, setSetupState] = useState<SetupState>({ bio: "", skills: [] as string[], pfp_url: "", pfp_file: null as null|File});
+    const [loginState, setLoginState] = useState<LoginState>({ user_name: "", password: "" });
+    const [signupState, setSignupState] = useState<SignupState>({ full_name: "", email: "", user_name: "", password: "" });
+    const [setupState, setSetupState] = useState<SetupState>({ bio: "", skills: [] as string[], pfp_url: "", pfp_file: null as null | File });
+    const login = async() => {
+        const res = await myAxios.post("/api/login", loginState);
+        return res.data;
+    }
+    const values = {
+        loginState,
+        setLoginState,
+        signupState,
+        setSignupState,
+        setupState,
+        setSetupState,
+        login
+    };
 
-  const values = {
-    loginState,
-    setLoginState,
-    signupState,
-    setSignupState,
-    setupState,
-    setSetupState
-  };
-
-  return (
-    <AuthFormContext.Provider value={values}>
-      {children}
-    </AuthFormContext.Provider>
-  );
+    return (
+        <AuthFormContext.Provider value={values}>
+            {children}
+        </AuthFormContext.Provider>
+    );
 };
 
 // A custom hook to use the context
 export const useAuthFormContext = (): AuthContextType => {
-  const context = useContext(AuthFormContext);
-  if (!context) {
-    throw new Error("useAuthFormContext must be used within an AuthFormContextProvider");
-  }
-  return context;
+    const context = useContext(AuthFormContext);
+    if (!context) {
+        throw new Error("useAuthFormContext must be used within an AuthFormContextProvider");
+    }
+    return context;
 };
